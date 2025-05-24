569f9877f33881fd026cdaaf75c8f456fded1f9a
sumeshi3648 <ulanazhusip@gmail.com>
2025-05-24 20:59:20 +0600

    added theme changing (dark and light mode)

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const newTodoInput = document.querySelector('.new-todo');
     const tasksList = document.querySelector('.tasks-list');
-    const checkboxes = document.querySelectorAll('.todo-item input[type="checkbox"]');
+    const body = document.body;
+    const themeToggleImg = document.getElementById('theme-toggle-img');
 
     function handleCheckboxChange(event) {
         const todoItem = event.target.closest('.todo-item');
@@ -23,10 +24,37 @@ document.addEventListener('DOMContentLoaded', () => {
         const span = document.createElement('span');
         span.textContent = text;
 
+        const img = document.createElement('img');
+        img.src = "assets/delete.png";
+        img.alt = "Delete todo item";
+        img.addEventListener('click', handleDeleteClick);
+
         listItem.appendChild(checkbox);
         listItem.appendChild(span);
+        listItem.appendChild(img);
         tasksList.appendChild(listItem);
     }
+
+    function handleDeleteClick(event) {
+        const todoItemToDelete = event.target.closest('.todo-item');
+        if (todoItemToDelete) {
+            tasksList.removeChild(todoItemToDelete);
+        }
+    }
+
+    function toggleTheme() {
+        if (body.classList.contains('dark-mode')) {
+            body.classList.remove('dark-mode')
+            themeToggleImg.src = 'assets/moon.png';
+            themeToggleImg.alt = 'toggle dark mode';
+        } else {
+            body.classList.add('dark-mode')
+            themeToggleImg.src = 'assets/sun.png';
+            themeToggleImg.alt = 'toggle light mode';
+        }
+    }
+
+    themeToggleImg.addEventListener('click', toggleTheme);
 
     newTodoInput.addEventListener('keydown', (event) => {
         if (event.key === "Enter") {
